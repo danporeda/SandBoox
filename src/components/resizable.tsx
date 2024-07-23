e0cff5fdfc23a37d01e33559571a7b7c07bef960
Dan <danporeda@yahoo.com>
2024-07-23 17:18:01 -0400

    props and logic added to Resizable, as well as css, to allow horizontal dragging

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,5 +1,5 @@
 import './resizable.css';
-import { ResizableBox } from 'react-resizable';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
@@ -7,14 +7,29 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === 'horizontal') {
+    resizableProps = {
+      className: 'resize-horizontal',
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+      resizeHandles: ['e'],
+    };
+  } else {
+    resizableProps = {
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      minConstraints: [Infinity, 24],
+      height: 300 ,
+      width: Infinity,
+      resizeHandles: ['s'],
+    }
+  }
+
   return (
-    <ResizableBox 
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      minConstraints={[Infinity, 24]}
-      height={300} 
-      width={Infinity}
-      resizeHandles={['s']}
-    >
+    <ResizableBox {...resizableProps}>
       {children}
     </ResizableBox>
   )
