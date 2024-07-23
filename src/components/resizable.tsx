867f26306f514e4524ef0cffa1303251034f03c2
Dan <danporeda@yahoo.com>
2024-07-23 16:28:31 -0400

    added maxConstraints and minConstraints to y-axis resizing

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -9,6 +9,8 @@ interface ResizableProps {
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   return (
     <ResizableBox 
+      maxConstraints={[Infinity, window.innerHeight * 0.9]}
+      minConstraints={[Infinity, 24]}
       height={300} 
       width={Infinity}
       resizeHandles={['s']}
