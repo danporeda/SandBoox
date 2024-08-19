be2d6d1d7970c7778e519ee93639523cf7da22f4
Dan <danporeda@yahoo.com>
2024-08-19 15:08:22 -0400

    started try-catch logic for get and post of cells in router. created new action types. TODO: create Actions

--- packages/local-api/dist/routes/cells.js
+++ packages/local-api/dist/routes/cells.js
@@ -14,11 +14,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createCellsRouter = void 0;
 const express_1 = __importDefault(require("express"));
+const promises_1 = __importDefault(require("fs/promises"));
+const path_1 = __importDefault(require("path"));
 const createCellsRouter = (filename, dir) => {
     const router = express_1.default.Router();
+    router.use(express_1.default.json());
+    const fullPath = path_1.default.join(dir, filename);
     router.get('/cells', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        const isLocalApiError = (err) => {
+            return typeof err.code === "string";
+        };
+        try {
+            const result = yield promises_1.default.readFile(fullPath, { encoding: 'utf-8' });
+            res.send(JSON.parse(result));
+        }
+        catch (err) {
+            if (isLocalApiError(err)) {
+                if (err.code === 'ENOENT') {
+                    yield promises_1.default.writeFile(fullPath, '[]', 'utf-8');
+                    res.send([]);
+                }
+            }
+            else {
+                throw err;
+            }
+        }
     }));
     router.post('/cells', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        const { cells } = req.body;
+        yield promises_1.default.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+        res.send({ status: 'ok' });
     }));
     return router;
 };
